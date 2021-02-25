35f3d69d3b05aee59f7fa06b8435ee98aaf7b9a3
Viktor <viktor.tholen@gmail.com>
2021-02-25 09:19:52 +0100

    fix

--- covid-app/src/components/Map.js
+++ covid-app/src/components/Map.js
@@ -1,44 +1,50 @@
-import { geoNaturalEarth1, geoPath, geoGraticule } from 'd3';
+//import { geoMiller, geoEquirectangular, geoNaturalEarth1, geoPath, geoGraticule } from 'd3';
 import * as d3 from "d3";
+import { useEffect } from 'react';
+import * as topojson from 'topojson-client';
 
-const projection = geoNaturalEarth1();
-const path = geoPath(projection);
-const graticule = geoGraticule();
+const projection = d3.geoEquirectangular();
+const path = d3.geoPath(projection);
+const graticule = d3.geoGraticule();
 
-const Map = ({ data: {land, interiors} }) => {
+const Map = ({ data: { land, borders } }) => {
 
-  function renderMap() {
+  useEffect(() => {
+    renderMap();
+  }, []);
+  const renderMap = () =>  {
     const width = window.innerWidth * 0.9, height = window.innerHeight * 0.9; //1600, height = 1000;
 
     // const redraw = () => { 
     //     svg.attr("transform",d3.event.transform);// "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
     // }
-
-    const svg = d3.select("body").append("svg")
+    
+    const svg = d3.select(".world_map").append("svg")
     .attr("width", width)
     .attr("height", height)
     .style("margin-top", window.innerHeight * 0.05)
-    .style("margin-left", window.innerWidth * 0.05)
-    .style("background-color","#eee");
+    .style("margin-left", window.innerWidth * 0.05);
     // .call(
     //   d3.zoom().on("zoom", redraw)
     // )
-    var g = svg.append("g");
+     var g = svg.append("g");
+     console.log(land.features);
+     console.log(borders);
 
     g.selectAll("path")
-        .data(land.features)
+        .data(land)
         .enter()
         .append("path")
         .attr("class","continent")
         .attr("d", path);
 
-    g.append("path")
-        .attr("class","interiors")
-        .attr("d", path(interiors));
+    // g.append("path")
+    //     .attr("class","borders")
+    //     .attr("d", path(borders));
   }
 
-  console.log(land);
-  console.log(interiors);
+  // console.log(land);
+  // console.log(borders);
   // return (<g className="marks">
   //   <path className="sphere" d={path({ type: 'Sphere' })} />
 
@@ -47,13 +53,12 @@ const Map = ({ data: {land, interiors} }) => {
   //   {land.features.map((feature,i) => (
   //     <path className="land" key={i} d={path(feature)} />
   //   ))}
-  //   <path className="interiors" d={path(interiors)} />
+  //   <path className="borders" d={path(borders)} />
   // </g>);
   return (
-  <div>
-    <div id = "world_map"></div>
-    {renderMap()}
-  </div>
+
+    <div className="world_map" ></div>
+
   )
 };
 
--- covid-app/src/components/useData.js
+++ covid-app/src/components/useData.js
@@ -1,24 +1,38 @@
 import '../App.css';
 import React, {useEffect, useState} from 'react';
 // import * as d3 from 'd3';
-import { json } from 'd3';
-import data from '../data/country_vaccinations.csv';
-import { feature, mesh } from "topojson-client";
+import * as d3 from 'd3';
+import topomap from '../data/world_countries.json';
+import worldmap from '../data/map.json';
+import statistics from '../data/country_vaccinations.csv';
+import test from '../data/test.json';
 
+import { feature, mesh } from "topojson-client";
+console.log(worldmap);
 const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json';
+// const jsonUrl = 'https://gist.githubusercontent.com/d3noob/5193723/raw/world-110m2.json';
 
 const useData = () => {
   const [data, setData] = useState(null);
-  console.log(data);
+  const [covidData, setCovidData] = useState(null);
+
 
   useEffect(() => {
-    json(jsonUrl).then(topology => {
-      const { countries, land } = topology.objects;
+      console.log(worldmap);
+      //const { countries, land } = topomap.features;
       setData({
-        land: feature(topology, land),
-        interiors: mesh(topology, countries, (a, b) => a !== b)
+        land: worldmap.features
+        //borders: mesh(topomap, countries, (a, b) => a !== b)
       });
-    });
+      //setData(topology);
+    // json(topomap).then((topology) => {
+    //   setData(topology);
+    // });
+
+    // csv(statistics).then(stats => {
+    //   setCovidData(stats);
+    // });
+    return () => undefined;
   }, []);
 
   return data;
